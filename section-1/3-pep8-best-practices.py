cdf129df0059056cd9cfe4f376f024411fb07a48
JlFerrete <joseluisferrete@gmail.com>
2021-04-24 02:32:28 +0200

    Pep8 best practices Part 1

---
+++ section-1/3-pep8-best-practices.py
@@ -0,0 +1,31 @@
+# Aplicando buenas prácticas Parte 1
+
+# 1.- Uso de la sangría
+
+if 5 > 2:
+    print("mensaje de 5 mayor a 2")
+
+# 2.- Código máximo por linea 79 carácteres máximo
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+      "aaaaaaaaaaaaaaaaaaaaaaaaaa")
+# Código de comentario por línea 72 caracteres máximo
+
+# 3.- Saltos de línea
+
+# 3.1.- Clases
+
+
+class MyClass:
+    def first_function():
+        return "first"
+
+    def second_function():
+        return "second"
+
+
+def principal_function():
+    print("Principal function")
+
+
+def principal_function():
+    print("Principal function")

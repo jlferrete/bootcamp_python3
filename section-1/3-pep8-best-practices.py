17cf5f61aff7551686bd8e16fd82649c8449d931
JlFerrete <joseluisferrete@gmail.com>
2021-04-25 02:02:18 +0200

    Pep8 Best practices

--- section-1/3-pep8-best-practices.py
+++ section-1/3-pep8-best-practices.py
@@ -3,11 +3,11 @@
 # 1.- Uso de la sangría
 
 if 5 > 2:
-    print("mensaje de 5 mayor a 2")
+    print("Mensaje de 5 mayor a 2")
 
-# 2.- Código máximo por linea 79 carácteres máximo
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-      "aaaaaaaaaaaaaaaaaaaaaaaaaa")
+# 2.- Código máximo por línea 79 caracteres máximo
+print("dddddddddddddddddddddddddddddddddddddddddddddd" +
+      " dddddddddddddddddddddddddddddddddddddddddd")
 # Código de comentario por línea 72 caracteres máximo
 
 # 3.- Saltos de línea
@@ -16,16 +16,47 @@ print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
 
 
 class MyClass:
+    # 3.1.1.- Class functions
     def first_function():
         return "first"
 
     def second_function():
         return "second"
 
-
+# 3.2.- Principal functions
 def principal_function():
     print("Principal function")
 
-
-def principal_function():
+# 4.- Aplicando lower_snake_case a la función
+def second_function():
     print("Principal function")
+
+# Aplicando en una variable
+
+lower_snake_case_value = 2
+
+# 5.- Clase nombre aplica UpperCase
+
+class SecondClass:
+    def _private_function():
+        print("Private function")
+
+    def public_function():
+        print("public function")
+
+# 6.- Sumas
+one = 1
+two = 344
+three = 10
+total = ( one
+        + two
+        - three )
+# 335
+
+one = "1111"
+two = "344"
+three = "333333"
+total = ( one
+        + two
+        - three )
+print(total) # 1111344333333
